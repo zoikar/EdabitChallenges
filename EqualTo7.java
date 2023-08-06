b5d1388ed7df9dc4ae34c41e0b6b996db0c40cdd
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:32:35 +0300

    Create EqualTo7.java

---
+++ EqualTo7.java
@@ -0,0 +1,11 @@
+/* Fix the code in the Code tab so the function returns true if and only if x is equal to 7. */
+
+public final class EqualTo7 {
+  public static boolean isSeven(int x) {
+		if(x==7){
+			return true;
+		}else{
+			return false;
+		}
+  }
+}
