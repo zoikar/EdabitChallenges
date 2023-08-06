89e844a2f81b891e87c987e39bf10ddeeae7cbc9
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:31:02 +0300

    Create ReturnSomething.java

---
+++ ReturnSomething.java
@@ -0,0 +1,7 @@
+/* Write a function that returns the string "something" joined with a space " " and the given argument a. */
+
+public class ReturnSomething {
+	public static String giveMeSomething(String a) {
+		return "something " + a;
+	}
+}
