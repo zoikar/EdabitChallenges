63547cbc8d531b582e92e1a809aa683ffa8caec1
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:40:42 +0300

    Create RemainderReturn.java

---
+++ RemainderReturn.java
@@ -0,0 +1,8 @@
+/* There is a single operator in Java, capable of providing the remainder of a division operation. Two numbers are passed as parameters. 
+The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value. */
+
+public class RemainderReturn {
+  public static int remainder(int a, int b) {
+		return a%b;
+  }
+}
