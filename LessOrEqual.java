06cf38ce1fe0248c524f54fbd1c6c6dd2510d6c4
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:50:02 +0300

    Create LessOrEqual.java

---
+++ LessOrEqual.java
@@ -0,0 +1,11 @@
+/* Create a method that takes an integer as its only argument and returns true if it's less than or equal to zero, otherwise return false. */
+
+public class LessOrEqual {
+    public static boolean lessThanOrEqualToZero(int num) {
+      if(num<=0){
+				return true;
+			}else{
+				return false;
+			}
+    }
+}
