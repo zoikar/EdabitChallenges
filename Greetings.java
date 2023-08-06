0306dcc10f6e1a80a1957203fc402af4110db330
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:43:52 +0300

    Create Greetings.java

---
+++ Greetings.java
@@ -0,0 +1,8 @@
+/* Create a function that takes a name and returns a greeting in the form of a string. */
+
+public class Greetings {
+	public static String helloName(String name) {
+		return "Hello " + name + "!";
+	}
+}
+
