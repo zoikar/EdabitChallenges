dd5fcef47901296b9e62ae18bf4f4a9a2e7412fd
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:33:56 +0300

    Create EqualNumbers.java

---
+++ EqualNumbers.java
@@ -0,0 +1,11 @@
+/* Create a function that takes two integers and checks if they are equal. */
+
+public class EqualNumbers {
+	public static boolean isEqual(int num1, int num2) {
+		if(num1 == num2){
+			return true;
+		}else{
+			return false;
+		}
+  }
+}
