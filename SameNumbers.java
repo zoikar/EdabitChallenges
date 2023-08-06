d708650732ba23ecdeea26b2dad5d44c1ee3bf9e
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:36:26 +0300

    Create SameNumbers.java

---
+++ SameNumbers.java
@@ -0,0 +1,11 @@
+/* Create a function that returns true when x is equal to y; otherwise return false. */
+
+public class SameNumbers {
+	public static boolean isSameNum(int x, int y) {
+		if(x==y){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
