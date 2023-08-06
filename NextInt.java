c1b26b55886764279eb5bcb78cad9d3fd3cae2dc
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:28:59 +0300

    Create NextInt.java

---
+++ NextInt.java
@@ -0,0 +1,7 @@
+/*Create a function that takes a number as an argument, increments the number by +1 and returns the result.*/
+
+public class NextInt {
+	public static int addition(int num) {
+		return num+1;
+	}
+}
