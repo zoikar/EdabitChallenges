fca8d5a931637dc19bc97ee58436ce294130694a
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-06 19:47:55 +0300

    Create Polygon.java

---
+++ Polygon.java
@@ -0,0 +1,7 @@
+/* Given an n-sided regular polygon n, return the total sum of internal angles (in degrees). */
+
+public class Polygon { 
+	public static int sumOfAngles(int n) {
+		return (n - 2) * 180;
+	}
+}
